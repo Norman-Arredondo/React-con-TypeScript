a2904654f021247399033f086020a936560f876a
Norman Arredondo <arredondo_norman25@outlook.es>
2023-05-24 23:07:17 -0600

    Tipos básicos con TS
    Reglas de validación

--- reforzamiento/src/typescript/TiposBasicos.tsx
+++ reforzamiento/src/typescript/TiposBasicos.tsx
@@ -1,8 +1,21 @@
 
 export const TiposBasicos = () => {
+
+    let nombre: string | number = 'nombre';
+    nombre = 'Norman';
+
+    const edad: number = 24;
+    const estaActivo: boolean = true;
+    const poderes: (string|number)[] = ['velocidad', 'volar', 'Fuerza'];
+
+
   return (
     <>
     <h3>Tipos Básicos</h3>
+
+    {nombre}, {edad}, {(estaActivo) ? 'activo' : 'No activo'}
+    <br/>
+    {poderes.join(', ')}
     </>
   )
 }
